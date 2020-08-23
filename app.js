ba760231d11c732afd09541781371bfb7e7922cf
Mariana Sakamoto <43222644+marisakamoto@users.noreply.github.com>
2020-08-23 00:27:09 -0300

    desafio feito

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-// let meuNome = 'Mariana'
-// let sobreNome = 'Sakamoto'
-// let textoResultado_p = document.querySelector("#result").innerText
-//     /* <p id="result">Pedra (user) ganha de Tesoura (comp) </p> */
-// let textoResultado_p = document.getElementById("userScore")
-
-
-let pontuacaoUsuario = 10;
-let pontuacaoComputador = 50;
+let pontuacaoUsuario = 0;
+let pontuacaoComputador = 0;
+let action = '';
 
 const userScore_span = document.querySelector('#userScore');
 const compScore_span = document.querySelector('#compScore');
@@ -16,10 +10,9 @@ const paperButton_img = document.querySelector('#paperButton');
 const rockButton_img = document.querySelector('#rockButton');
 const scissorsButton_img = document.querySelector('#scissorsButton');
 
-document.querySelector('#userScore').innerText = pontuacaoUsuario;
-document.querySelector('#compScore').innerText = pontuacaoComputador;
 
-jogadasPossiveis = ['rock', 'paper', 'scissors'];
+
+jogadasPossiveis = ['pedra', 'papel', 'tesoura'];
 
 
 function jogo(cliqueDoUsuario) {
@@ -27,42 +20,85 @@ function jogo(cliqueDoUsuario) {
     console.log(cliqueDoUsuario);
     n = Math.floor(Math.random() * 3);
     cliqueDoComputador = jogadasPossiveis[n];
+    paperButton_img.classList.remove('green-glow');
+    paperButton_img.classList.remove('red-glow');
+    paperButton_img.classList.remove('gray-glow');
+
+    rockButton_img.classList.remove('green-glow');
+    rockButton_img.classList.remove('red-glow');
+    rockButton_img.classList.remove('gray-glow');
+
+    scissorsButton_img.classList.remove('green-glow');
+    scissorsButton_img.classList.remove('red-glow');
+    scissorsButton_img.classList.remove('gray-glow');
 
     switch (cliqueDoUsuario + cliqueDoComputador) {
-        case cliqueDoUsuario:
-            ganhador = Empate;
+
+        case 'papelpapel':
+            paperButton_img.classList.add('gray-glow');
+            action = 'empata';
+            pontuacaoComputador = pontuacaoComputador + 1;
+            pontuacaoUsuario = pontuacaoUsuario + 1;
+            break;
+        case 'pedrapedra':
+            rockButton_img.classList.add('gray-glow');
+            action = 'empata';
+            pontuacaoComputador = pontuacaoComputador + 1;
+            pontuacaoUsuario = pontuacaoUsuario + 1;
+            break;
+        case 'tesouratesoura':
+            scissorsButton_img.classList.add('gray-glow');
+            action = 'empata';
+            pontuacaoComputador = pontuacaoComputador + 1;
+            pontuacaoUsuario = pontuacaoUsuario + 1;
             break;
 
-        case
+
+        case 'papeltesoura':
+            paperButton_img.classList.add('red-glow');
+            action = 'perde';
+            pontuacaoComputador = pontuacaoComputador + 1;
+            break;
+        case 'pedrapapel':
+            rockButton_img.classList.add('red-glow');
+            action = 'perde';
+            pontuacaoComputador = pontuacaoComputador + 1;
+            break;
+        case 'tesourapedra':
+            scissorsButton_img.classList.add('red-glow');
+            action = 'perde';
+            pontuacaoComputador = pontuacaoComputador + 1;
+            break;
+        case 'papelpedra':
+            paperButton_img.classList.add('green-glow');
+            action = 'ganha';
+            pontuacaoUsuario = pontuacaoUsuario + 1;
+            break;
+        case 'pedratesoura':
+            rockButton_img.classList.add('green-glow');
+            action = 'ganha';
+            pontuacaoUsuario = pontuacaoUsuario + 1;
+            break;
+        case 'tesourapapel':
+            scissorsButton_img.classList.add('green-glow');
+            action = 'ganha';
+            pontuacaoUsuario = pontuacaoUsuario + 1;
+            break;
 
     }
+    document.querySelector('#result').innerText = cliqueDoUsuario + ' ' + action + ' de ' + cliqueDoComputador;
+    document.querySelector('#userScore').innerText = pontuacaoUsuario;
+    document.querySelector('#compScore').innerText = pontuacaoComputador;
 
 }
 
 function principal() {
-    paperButton_img.addEventListener("click", () => jogo('paperButton'));
-    rockButton_img.addEventListener("click", () => jogo('rockButton'));
-    scissorsButton_img.addEventListener("click", () => jogo('scissorsButton'));
+
+    paperButton_img.addEventListener("click", () => jogo('papel'));
+    rockButton_img.addEventListener("click", () => jogo('pedra'));
+    scissorsButton_img.addEventListener("click", () => jogo('tesoura'));
+
+
 
 }
 principal();
-switch (ganhador) {
-    case 'comp':
-        console.log('Máquina venceu');
-        break;
-    case 'user':
-        console.log('Humano venceu');
-        break;
-    default:
-        console.log('Empate técnico');
-}
-
-
-let fruta = 'Papaya';
-switch (fruta) {
-    case 'Laranja':
-        console.log('Laranjaaaaa!');
-        break;
-    default:
-        console.log('Não há ' + fruta + ' hoje');
-}
